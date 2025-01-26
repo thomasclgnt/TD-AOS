4fb7b47379d2b7d075da0be64f2bb1d8a2ec066f
mariemclff <mariemecaliff@gmail.com>
2025-01-26 13:47:24 +0100

    class Role

---
+++ RESTProject/src/main/java/fr/insa/soa/RESTProject/User/Role.java
@@ -0,0 +1,27 @@
+package fr.insa.soa.RESTProject.User;
+
+public enum Role {
+    DEMANDEUR("demandeur"),
+    BENEVOLE("benevole"),
+    VALIDEUR("valideur");
+
+    private final String roleName;
+
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public static Role fromString(String roleName) {
+        for (Role role : Role.values()) {
+            if (role.getRoleName().equalsIgnoreCase(roleName)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Role invalide : " + roleName + ". Essayez à nouveau.");
+    }
+}
+
