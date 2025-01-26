e8736c91c42222fa7012aa1fe928972b48c57fe6
mariemclff <mariemecaliff@gmail.com>
2025-01-26 15:17:00 +0100

    modif demande

--- RESTProject/src/main/java/fr/insa/soa/RESTProject/Demande/DemandeResource.java
+++ RESTProject/src/main/java/fr/insa/soa/RESTProject/Demande/DemandeResource.java
@@ -15,12 +15,34 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("Demande")
-public class DemandeRessource {
+@Path("demandes")
+public class DemandeResource {
 	
 	private static final ArrayList<Demande> demandes = new ArrayList<>();
 
- // Créer une nouvelle demande
+	// Obtenir toutes les demandes
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Demande> getDemandes() {
+        return demandes;
+    }
+
+    // Rechercher une demande par ID
+    @GET
+    @Path("/{idDemande}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Demande rechercherDemandeParId(@PathParam("idDemande") int idDemande) {
+    	
+        for (Demande demande : demandes) {
+            if (demande.getId() == idDemande) {
+                return demande;
+            }
+        }
+        
+        return null; // Retourne null si la demande n'est pas trouvée
+    }
+	
+    // Créer une nouvelle demande
     @POST
     @Path("/new")
     public Demande creerDemande(@QueryParam("description") String description) {
@@ -28,114 +50,87 @@ public class DemandeRessource {
     	int id = demandes.size()+1;
     	User usertest = new User(10, "thomas", "thomas@mail.com", "demandeur");
         Demande demande = new Demande(id, description, usertest, Statut.CREEE);
-//        Demande demande = new Demande(id, description, demandeur, Statut.CREEE);
+        // Demande demande = new Demande(id, description, demandeur, Statut.CREEE);
         demandes.add(demande);
         return demande;
         
     }
     
-// // Valider une demande
-//    @PUT
-//    @Path("/validate/{id}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public boolean validerDemande(@PathParam("id") int idDemande, User valideur) {
-//    	
-//        for (Demande demande : demandes) {
-//        	
-//            if (demande.getId() == idDemande && demande.getStatut() == Statut.CREEE) {
-//            	
-//                demande.setValideur(valideur);
-//                demande.setStatut(Statut.VALIDEE);
-//                return true;
-//                
-//            }
-//        }
-//        return false; // Échec si la demande n'est pas trouvée ou déjà traitée
-//        
-//    }
+    // Valider une demande
+    @PUT
+    @Path("/validate/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean validerDemande(@PathParam("id") int idDemande) {
+    	
+    	User valideur = new User(10, "thomas", "thomas@mail.com", "valideur");
+        for (Demande demande : demandes) {
+            if (demande.getId() == idDemande && demande.getStatut() == Statut.CREEE) {
+                demande.setValideur(valideur);
+                demande.setStatut(Statut.VALIDEE);
+                return true;
+            }
+        }
+        
+        return false; // Échec si la demande n'est pas trouvée ou déjà traitée
+        
+    }
     
- // Refuser une demande
-//    @PUT
-//    @Path("/refuse/{id}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public boolean refuserDemande(@PathParam("id") int idDemande, User valideur, String motifRefus) {
-//    	
-//        for (Demande demande : demandes) {
-//        	
-//            if (demande.getId() == idDemande && demande.getStatut() == Statut.CREEE) {
-//            	
-//                demande.setValideur(valideur);
-//                demande.setMotifRefus(motifRefus);
-//                demande.setStatut(Statut.REFUSEE);
-//                
-//                return true;
-//                
-//            }
-//        }
-//        return false; // Échec si la demande n'est pas trouvée ou déjà traitée
-//    }
+    // Refuser une demande
+    @PUT
+    @Path("/refuse/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean refuserDemande(@PathParam("id") int idDemande, @QueryParam("motif") String motifRefus) {
+    	
+    	User valideur = new User(10, "thomas", "thomas@mail.com", "valideur");
+    	
+        for (Demande demande : demandes) {
+            if (demande.getId() == idDemande && demande.getStatut() == Statut.CREEE) {
+                demande.setValideur(valideur);
+                demande.setMotifRefus(motifRefus);
+                demande.setStatut(Statut.REFUSEE);
+                
+                return true;
+            }
+        }
+        return false; // Échec si la demande n'est pas trouvée ou déjà traitée
+    }
     
- // Annuler une demande
+    // Annuler une demande
     @PUT
     @Path("/cancel/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public boolean annulerDemande(@PathParam("id") int idDemande) {
     	
+    	User valideur = new User(10, "thomas", "thomas@mail.com", "valideur");
+    	
         for (Demande demande : demandes) {
-        	
             if (demande.getId() == idDemande) {
-            	
+            	demande.setValideur(valideur);
                 demande.setStatut(Statut.ANNULEE);
+                
                 return true;
             }
         }
         return false; // Échec si la demande n'est pas trouvée ou non annulable
     }
     
-// // Assigner un travailleur à une demande
-//    @PUT
-//    @Path("/assignWorker/{id}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public boolean assignerBenevole(@PathParam("id") int idDemande, User benevole) {
-//    	
-//        for (Demande demande : demandes) {
-//            if (demande.getId() == idDemande && demande.getStatut() == Statut.CREEE) {
-//                
-//            	demande.setBenevole(benevole);
-//            	
-//                demande.setStatut(Statut.PRISE_EN_CHARGE);
-//                
-//                return true;
-//            }
-//        }
-//        return false; // Échec si la demande n'est pas trouvée ou non assignable
-//        
-//    }
-    
- // Obtenir toutes les demandes
-    @GET
-    @Path("/getall")
+    // Assigner un travailleur à une demande
+    @PUT
+    @Path("/assignWorker/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Demande> getDemandes() {
+    public boolean assignerBenevole(@PathParam("id") int idDemande) {
     	
-        return demandes;
-        
-    }
-
-    // Rechercher une demande par ID
-    @GET
-    @Path("/getone/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Demande rechercherDemandeParId(@PathParam("id") int idDemande) {
+    	User benevole = new User(10, "thomas", "thomas@mail.com", "benevole");
     	
         for (Demande demande : demandes) {
-        	
-            if (demande.getId() == idDemande) {
-            	
-                return demande;
+            if (demande.getId() == idDemande && demande.getStatut() == Statut.CREEE) {
+            	demande.setBenevole(benevole);
+                demande.setStatut(Statut.PRISE_EN_CHARGE);
+                return true;
             }
         }
-        return null; // Retourne null si la demande n'est pas trouvée
+        return false; // Échec si la demande n'est pas trouvée ou non assignable
+        
     }
     
 }
--- RESTProject/src/main/webapp/index.jsp
+++ RESTProject/src/main/webapp/index.jsp
@@ -1,7 +1,9 @@
 <html>
 <body>
-    <h2>Jersey RESTful Web Application!</h2>
+    <h2>Welcome to the International Red Cross application!</h2>
     <p><a href="webapi/myresource">Jersey resource</a>
+    <p><a href="webapi/users">Gestion des utilisateurs !</a>
+    <p><a href="webapi/demandes">Gestion des demandes !</a>
     <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
     for more information on Jersey!
 </body>
